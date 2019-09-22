97861c25e95a3c0b5ce03f60375285732f576466
Luis <lu15bytes@gmail.com>
2019-09-22 03:37:40 -0500

    ADDED: imageSlider component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React,{Suspense, lazy} from 'react';
-import {Container} from 'react-bootstrap';
+import {Container} from 'react-bootstrap';  
 import './App.css';
 import Nvbar from './components/nvbar/Nvbar';
+import ImageSlider from './components/imageSlider/ImageSlider';
 const Gallery = lazy(() => import('./components/gallery/Gallery'));
+
 
 function App() {
   return (
     <Container>
           <Nvbar/>
+          <ImageSlider/>
           <Suspense fallback={<div>Loading...</div>}>
            <Gallery/>
           </Suspense>
