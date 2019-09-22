eac332155adc04e91cbbfa38b0c3dff3622bac30
Luis <lu15bytes@gmail.com>
2019-09-22 17:33:08 -0500

    ADDED: gallery and set styles

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,73 +1,94 @@
 import React from 'react';
 import Set from '../set/Set';
+import './gallery.css';
 
 const sets = [
     {
         id: 1,
+        girlName: 'Susan',
         img: "https://picsum.photos/id/1/5616/3744",
         title: "pack",
-        desc: "something"
+        desc: "something",
+        price: 10
     },
     {
         id: 2,
+        girlName: 'Susan',
         img: "https://picsum.photos/id/2/5616/3744",
         title: "pack",
-        desc: "something"
+        desc: "something",
+        price: 10
     },
     {
         id: 3,
+        girlName: 'Susan',
         img: "https://picsum.photos/id/3/5616/3744",
         title: "pack",
-        desc: "something"
+        desc: "something",
+        price: 10
     },
     {
         id: 4,
+        girlName: 'Susan',
         img: "https://picsum.photos/id/4/5616/3744",
         title: "pack",
-        desc: "something"
+        desc: "something",
+        price: 10
     },
     {
         id: 5,
+        girlName: 'Susan',
         img: "https://picsum.photos/id/1011/5472/3648",
         title: "pack",
-        desc: "something"
+        desc: "something",
+        price: 10
     },
     {
         id: 6,
+        girlName: 'Susan',
         img: "https://picsum.photos/id/6/5616/3744",
         title: "pack",
-        desc: "something"
+        desc: "something",
+        price: 10
     },
     {
         id: 7,
+        girlName: 'Susan',
         img: "https://picsum.photos/id/1008/5616/3744",
         title: "pack",
-        desc: "something"
+        desc: "something",
+        price: 10
     },
     {
         id: 8,
+        girlName: 'Susan',
         img: "https://picsum.photos/id/8/5616/3744",
         title: "pack",
-        desc: "something"
+        desc: "something",
+        price: 10
     },
     {
         id: 9,
+        girlName: 'Susan',
         img: "https://picsum.photos/id/9/5616/3744",
         title: "pack",
-        desc: "something"
+        desc: "something",
+        price: 10
     },
     {
         id: 10,
+        girlName: 'Susan',
         img: "https://picsum.photos/id/1004/5616/3744",
         title: "pack",
-        desc: "something"
+        desc: "something",
+        price: 10
     }
 ]
 
 const Gallery = () => (
-  <div className="cities-grid">
+  <div className="cities-grid row">
     {sets.map((set) =>
-      <Set key={set.id} img={set.img} title={set.title} desc={set.desc} />
+      <Set key={set.id} img={set.img} title={set.title} girlName={set.girlName} desc={set.desc} price={set.price}/>
     )}
   </div>
 );
--- src/components/set/Set.jsx
+++ src/components/set/Set.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import {Card, Button} from 'react-bootstrap';
+import './set.css';
 
 const Set = (props) =>{
     return(
-        <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={props.img} />
-            <Card.Body>
-                <Card.Title>{props.title}</Card.Title>
-                <Card.Text>
-                    {props.desc}
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-        </Card>
+            <Card className="column">
+                <h4>{props.girlName}</h4>
+                <Card.Img variant="top" src={props.img} />
+                <Card.Body>
+                    <Card.Title>{props.title}</Card.Title>
+                    <Card.Text>
+                        {props.desc}
+                    </Card.Text>
+                    <p>{`$ ${props.price} MXN`}</p>
+                    <Button variant="primary">Buy</Button>
+                </Card.Body>
+            </Card>
     )
 }
 
