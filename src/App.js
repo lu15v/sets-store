ebf0608de148f5222c791a0a5cb3759168928c04
Luis <lu15bytes@gmail.com>
2019-09-22 03:24:59 -0500

    MOD: styling, container added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React,{Suspense, lazy} from 'react';
+import {Container} from 'react-bootstrap';
 import './App.css';
 import Nvbar from './components/nvbar/Nvbar';
 const Gallery = lazy(() => import('./components/gallery/Gallery'));
 
 function App() {
   return (
-    <div className="App">
+    <Container>
           <Nvbar/>
           <Suspense fallback={<div>Loading...</div>}>
            <Gallery/>
           </Suspense>
-    </div>
+    </Container>
   );
 }
 
