2edb584016cbca4d5c9b76dd3e56176ab049ed3e
Luis <lu15bytes@gmail.com>
2019-09-26 22:48:53 -0500

    MOD: login and tabLogin for including register comp

--- src/components/tabLogin/TabLogin.jsx
+++ src/components/tabLogin/TabLogin.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Tabs, Tab} from 'react-bootstrap';
 import Login from '../login/Login';
+import Register from '../register/Register';
+
 import './tabLogin.css';
 
 const TabLogin = () => {
@@ -8,7 +10,7 @@ const TabLogin = () => {
         <div className="tab-container">
             <Tabs defaultActiveKey="signIn" id="LogRegTab">
                 <Tab eventKey="signUp" title="SIGN UP">
-    
+                    <Register/>
                 </Tab>
                 <Tab eventKey="signIn" title="SIGN IN">
                     <Login/>
