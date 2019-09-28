2a8c0159a46cba608a65a862d8e9577ea69ef903
Luis <lu15bytes@gmail.com>
2019-09-28 14:12:58 -0500

    UPDATE: preparing login comp for async call

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -2,35 +2,57 @@ import React from 'react';
 import {Form, Button, Row, Col, Image} from 'react-bootstrap';
 import './login.css'
 
-const Login = () =>{
-    return(
-        <Row className="padding">
-            <Col>
-                <Form>
-                    <h3>Registered Users</h3>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                            We'll never share your personal info with anyone else.
-                        </Form.Text>
-                    </Form.Group>
+class Login extends React.Component{
 
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Form.Group controlId="formBasicCheckbox">
-                        <p>By clicking Sign In, you agree to our Terms of Use and our Privacy Policy.</p>
-                    </Form.Group>
-                    <Button className="button" type="submit">
-                        SIGN IN
-                    </Button>
-                    <Form.Group controlId="formBasicLink" className="padding-top">
-                        <a href="#" className="link">Forgot your password?</a>
-                    </Form.Group>
-                </Form>
-            </Col>
-        </Row>
-    );
+    constructor(){
+        super();
+        this.state = {
+            login: {
+                username: '',
+                password: ''
+            }
+        }
+    }
+    onSubmit(event, form){
+        event.preventDefault();
+    }
+    onChangeUsername = (event) =>{
+        this.setState({login: {...this.state.login, username: event.target.value}})
+    }
+    onChangePassword = (event) =>{
+        this.setState({login: {...this.state.login, password: event.target.value}})
+    }
+
+    render(){
+        return(
+            <Row className="padding">
+                <Col>
+                    <Form  onSubmit={(e) => this.onSubmit(e, this.state.login)}>
+                        <h3>Registered Users</h3>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Control type="email" placeholder="Enter email"  onChange={this.onChangeUsername} required/>
+                            <Form.Text className="text-muted">
+                                We'll never share your personal info with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+    
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Control type="password" placeholder="Password"  onChange={this.onChangePassword} required/>
+                        </Form.Group>
+                        <Form.Group controlId="formBasicCheckbox">
+                            <p>By clicking Sign In, you agree to our Terms of Use and our Privacy Policy.</p>
+                        </Form.Group>
+                        <Button className="button" type="submit">
+                            SIGN IN
+                        </Button>
+                        <Form.Group controlId="formBasicLink" className="padding-top">
+                            <a href="#" className="link">Forgot your password?</a>
+                        </Form.Group>
+                    </Form>
+                </Col>
+            </Row>
+        );
+    }
 }
 
 export default Login;
