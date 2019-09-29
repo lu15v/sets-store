eb189e3bfd3603c0e11b1916c6966050ac2bf493
Luis <lu15bytes@gmail.com>
2019-09-28 22:36:36 -0500

    ADDED: setPreview component

---
+++ src/components/setPreview/SetPreview.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Navbar from '../nvbar/Nvbar';
+import {Image, Button} from 'react-bootstrap';
+import './setPreview.css';
+
+class SetPreview extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div className="preview-container">
+                <Navbar/>
+                <Image className="img" src="https://picsum.photos/id/237/240/430"/>
+                <h1>Susan</h1>
+                <div>
+                    <p className="desc">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est.</p>
+                </div>
+                <div>
+                    <p className="desc content">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est.</p>
+                </div>
+                <Button className="button">Buy</Button>
+            </div>
+        );
+    }
+}
+
+
+export default SetPreview;
