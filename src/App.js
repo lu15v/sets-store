43388c46809396ee4d5d9280679410f7749fb962
Luis <lu15bytes@gmail.com>
2019-10-01 19:49:43 -0500

    FEATURE: redux configured

--- src/App.js
+++ src/App.js
@@ -4,21 +4,30 @@ import './App.css';
 import Menu from './containers/menu/Menu';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import TabLogin from './components/tabLogin/TabLogin';
-import Login from './components/login/Login';
+//import Login from './components/login/Login';
 import SetPreview from './components/setPreview/SetPreview';
+
+//redux
+import {reducer} from './redux/reducers/reducer';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+
+const store = createStore(reducer);
 
 function App() {
   return (
-    <Container>
-      <Router>
-        <Switch>
-          <Route exact path='/' component={Menu}/>
-          <Route exact path='/login' component={TabLogin}/>
-          <Route exact path='/set/:id' component={SetPreview}/>
-          {/*<Route component={DefaultPage} />*/}
-        </Switch>
-      </Router>
-    </Container>
+    <Provider store={store}>
+      <Container>
+        <Router>
+          <Switch>
+            <Route exact path='/' component={Menu}/>
+            <Route exact path='/login' component={TabLogin}/>
+            <Route exact path='/set/:id' component={SetPreview}/>
+            {/*<Route component={DefaultPage} />*/}
+          </Switch>
+        </Router>
+      </Container>
+    </Provider>
     // <TabLogin/>
   );
 }
---
+++ src/redux/reducers/reducer.js
@@ -0,0 +1,8 @@
+
+const initState = {
+
+};
+
+export const reducer = (state = initState, action) =>{
+    return state;
+}
