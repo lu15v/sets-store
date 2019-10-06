26e3bce1b295c7ac28f507ab946dbe283eed9fd1
Luis <lu15bytes@gmail.com>
2019-10-06 14:01:45 -0500

    FEATURE: terms component created

---
+++ src/components/terms/Terms.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Nvbar from '../nvbar/Nvbar';
+import './terms.css';
+
+const Terms = () =>{
+    return(
+        <>
+            <Nvbar/>
+            <div className="terms-text">
+                <h4 className="headrs">terms</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas elit nulla, maximus aliquam lacus blandit id. Aliquam orci neque, iaculis sed efficitur sed, varius sit amet felis. Maecenas a velit quis arcu tincidunt pharetra. Suspendisse quis malesuada eros, eu tincidunt felis. Donec cursus lacinia efficitur. Curabitur fringilla feugiat tincidunt. Nam ac molestie arcu, vitae bibendum turpis. Curabitur elit elit, ullamcorper ac pharetra vitae, tincidunt et est. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse vehicula sem nulla, ut cursus dui sagittis consectetur.</p>
+                
+                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed fermentum libero vitae malesuada ultrices. Nulla facilisi. Praesent blandit urna est. Sed congue aliquet elit, nec consectetur dolor dictum id. Vestibulum facilisis ligula risus, ut aliquam tortor accumsan vel. Suspendisse ornare vulputate efficitur. Praesent sit amet turpis dictum, consectetur quam in, bibendum massa. Fusce rutrum ligula at nulla euismod venenatis.</p>
+                <h4 className="headrs">terms2</h4>
+                <p>Sed interdum dui ac magna condimentum tempor. Nulla varius nulla at velit congue, ac suscipit mauris pretium. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas vitae viverra orci, ut suscipit lorem. Mauris faucibus finibus efficitur. Praesent vestibulum ante vulputate imperdiet mattis. Sed eget mollis magna. Mauris sit amet rhoncus metus. Mauris porttitor tortor at finibus vestibulum.</p>
+            </div>
+        </>
+    );
+}
+
+
+export default Terms;
