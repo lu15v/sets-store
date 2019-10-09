42ab21523b7075f1e5c4b997fe46208f8d0e3fe6
Luis <lu15bytes@gmail.com>
2019-10-08 23:16:02 -0500

    FEATURE: created generic component for displaying messages (implemented in cart when empty

---
+++ src/components/utilities/DisplayMessage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './displayMessage.css';
+
+const DisplayMessage = () =>{
+    return(
+        <div className="separation">
+            <div className="message">
+                <h4 className="main-message">Empty Cart</h4>
+                <p className="secondary-message">Start looking at our sets, we are sure you're gonna love them!</p>
+            </div>
+        </div>
+    )
+}
+
+
+export default DisplayMessage;
--- src/containers/cart/Cart.jsx
+++ src/containers/cart/Cart.jsx
@@ -4,7 +4,7 @@ import {ListGroup, Row, Col, Button} from 'react-bootstrap';
 import CartItem from './CartItem';
 import './cart.css';
 import {deleteItem} from '../../redux/actions/cart';
-
+import DisplayMessage from '../../components/utilities/DisplayMessage';
 import {connect} from 'react-redux';
 
 class Cart extends React.Component{
@@ -14,30 +14,34 @@ class Cart extends React.Component{
             return [...this.props.cartList.values()].reduce(function(acc, item) {return acc + item.price;},0)
         return 0;
     }
+    
+
     render(){
         return(
             <>
             <Nvbar/>
-            <Row className="cart-content">
-                <Col className="cart-list">
-                    <ListGroup variant="flush">
-                        {[...this.props.cartList.values()].map((item, idx) =>
-                            <CartItem key={idx} set={item} delFunc={this.props.deleteItem} />
-                        )}
-                    </ListGroup>
-                </Col>
-                <Col className="col-buy-section">
-                    <div className="buy-container">
-                        <Row>
-                            <Col xs={6} sm={6} md={6}><h4 className="subtotal">{`Subtotal (items ${this.props.cartList.size}):`} </h4></Col>
-                            <Col xs={6} sm={6} md={6}><h4 className="price">{`$ ${this.total()}`}</h4></Col>
-                        </Row>
-                        <Row className="cart-button-container">
-                            <Col><Button className="button-checkout">Checkout</Button></Col>
-                        </Row>
-                    </div>
-                </Col>
-            </Row>
+            {this.props.cartList.size > 0 ? 
+                <Row className="cart-content">
+                    <Col className="cart-list">
+                        <ListGroup variant="flush">
+                                {[...this.props.cartList.values()].map((item, idx) =>
+                                    <CartItem key={idx} set={item} delFunc={this.props.deleteItem} />
+                                )}
+                            </ListGroup> 
+                        </Col>
+                        <Col className="col-buy-section">
+                            <div className="buy-container">
+                                <Row>
+                                    <Col xs={6} sm={6} md={6}><h4 className="subtotal">{`Subtotal (items ${this.props.cartList.size}):`} </h4></Col>
+                                    <Col xs={6} sm={6} md={6}><h4 className="price">{`$ ${this.total()}`}</h4></Col>
+                                </Row>
+                                <Row className="cart-button-container">
+                                    <Col><Button className="button-checkout">Checkout</Button></Col>
+                                </Row>
+                            </div>
+                        </Col>
+                </Row>
+            : <DisplayMessage/>}
             </>
         );
     }
